7d4ab467646e95c37674a7981f64d74c1e78fb45
Phil Oye <philoye@philoye.com>
2011-04-19 10:10:03 +1000

    Using paul irish's image loaded plugin

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,50 @@
+// Source:
+// https://gist.github.com/268257
+//
+// $('img.photo',this).imagesLoaded(myFunction)
+// execute a callback when all images have loaded.
+// needed because .load() doesn't work on cached images
+
+// mit license. paul irish. 2010.
+// webkit fix from Oren Solomianik. thx!
+
+// callback function is passed the last image to load
+//   as an argument, and the collection as `this`
+
+
+$.fn.imagesLoaded = function(callback){
+  var elems = this.children('img'),
+      len   = elems.length;
+
+  elems.bind('load',function(){
+      if (--len <= 0){ callback.call(elems,this); }
+  }).each(function(){
+     // cached images don't fire load sometimes, so we reset src.
+     if (this.complete || this.complete === undefined){
+        var src = this.src;
+        // webkit hack from http://groups.google.com/group/jquery-dev/browse_thread/thread/eee6ab7b2da50e1f
+        // data uri bypasses webkit log warning (thx doug jones)
+        this.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
+        this.src = src;
+     }
+  });
+
+  return this;
+};
+
 $(document).ready(function() {
 
-  $(".snap").each(function() {
-    var $this = $(this);
-    if (Modernizr.csstransitions) {
-      setTimeout(function() {
+  $("#images").imagesLoaded(function() {
+    $(".snap").each(function() {
+      var $this = $(this);
+      if (Modernizr.csstransitions) {
+        setTimeout(function() {
+          $this.addClass("moved");
+        }, 1000 + Math.random()*2000);
+      } else {
         $this.addClass("moved");
-      }, 2000 + Math.random()*2000);
-    } else {
-      $this.addClass("moved");
-    }
+      }
+    });
   });
 
   $("script[type='text/x-jquery-template']").each( function () {
