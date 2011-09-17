655a4dbe4c6236489f08e9b122e4c0f7c2dfc5e9
Phil Oye <philoye@philoye.com>
2011-09-17 22:41:02 +1000

    eventwax changed the json structure.

--- app.rb
+++ app.rb
@@ -36,19 +36,18 @@ module ParisPictureSchool
       events = JSON.parse(RestClient.get("https://" + ENV['PARISPICTURESCHOOL'] + "@secure.eventwax.com/api/events.json"))
       @events = []
       events.each do |e|
-        session = e['event_sessions'].first
-        tix_remaining    = session['capacity'].to_i - session['attendees'].length.to_i
+        tix_remaining    = e['capacity'].to_i - e['attendees'].length.to_i
         tz = e['time_zone']['info']['identifier']
         event = {
           :name     => e['name'],
           :location => e['location'],
           :link     => "http://parispictureschool.eventwax.com/" + e['uri'] + "/register",
-          :date     => local_time(session['starts_on'], tz).strftime('%e %b %Y').strip,
-          :start    => local_time(session['starts_on'], tz).strftime('%l:%M%P').strip,
-          :end      => local_time(session['ends_on'], tz).strftime('%l:%M%P').strip,
+          :date     => local_time(e['starts_on'], tz).strftime('%e %b %Y').strip,
+          :start    => local_time(e['starts_on'], tz).strftime('%l:%M%P').strip,
+          :end      => local_time(e['ends_on'], tz).strftime('%l:%M%P').strip,
           :sold_out => ((tix_remaining == 0) ? true : false)
         }
-        @events.push event if future?(session['starts_on'], tz)
+        @events.push event if future?(e['starts_on'], tz)
       end
       @events.sort_by! {|event| Date.parse(event[:date]) }
       @events.to_json
